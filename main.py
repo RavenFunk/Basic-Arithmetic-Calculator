9565654e7f73e238d5fb346db22f099213227d45
RavenFunk <ravencfunk@gmail.com>
2026-07-21 15:43:45 -0400

    Greatly improved input handling, added power operation, and greatly improved the help command.

--- main.py
+++ main.py
@@ -9,58 +9,77 @@ def divide(a, b):
     return a / b
 def sqrt(a):
     return math.sqrt(a)
+def power(a, b):
+    return a**b
 
 def calculator():
     print("- - - - - - - - - - - - - - - - - - - - - - -\nInput 'help' to get a list of useful commands.")
     result = 0
     calculations = 0
     while True:
-        
-        userInput = input("> ").strip()
+        userInput = input("> ")
         if not userInput:
             continue
         parts = userInput.split()
         command = (parts[0]).strip().lower()
         if command == 'new':
             calculations = 0 
+            result = 0
             print()
             continue
         elif command == "help":
-            print("- - - - - - - - - Help - - - - - - - - - \n\n [1] new : Begins a new input, type [See 3] to calculate something.\n [2] quit/exit : Closes the calculator.\n [3] x [operation] x : Valid for any basic arithmetic, returns the answer. \n [4] [operation] x : Allows you to further calculate on the previous output. \n [5] sqrt x : Returns the square root of x, begins a new calculation. \n")
+            print("- - - - - - - - - HELP - - - - - - - - - \n\n - - - - - Commands - - - - - \n [1] new : Begins a new input, type [See 3] to calculate something.\n [2] quit/exit : Closes the calculator.\n\n - - - - - Operations - - - - -\n\n [3] x + y : Adds y to x. \n [4] x - y : Subtracts y from x. \n [5] x * y : Multiplies x by y. \n [6] x / y : Divides x by y. \n [7] x ^ y OR x ** y : Raises x to the power of y. \n [8] sqrt x : Returns the square root of x. Starts a new calculation. \n\n - - - - - General - - - - -\n [9] x [operation] y : Valid for any basic arithmetic, returns the answer. \n [10] [operation] x : Allows you to further calculate on the previous output. \n\n ")
             continue
         elif command in {"quit", "exit"}:
             print("Goodbye!")
             break
         elif command == "sqrt":
-            number = float(parts[1])
-            print(sqrt(number))
-            print("Ran command: 'new'\n")
-            calculations = 0
-            continue
-            
+            if len(parts) == 2:
+                try:
+                    number = float(parts[1])
+                except ValueError:
+                    print("Please enter a valid number\n")
+                    continue
+                
+                if number >= 0:
+                    
+                    print(sqrt(number))
+                    print("\nRan command: 'new'")
+                    calculations = 0
+                    result = 0
+                    continue
+                else:
+                    print("Please enter a number greater than or equal to zero.")
+                    continue
+            else:
+                print("Please enter a valid operation. Or enter 'help' to get a list of useful commands.\n")
+                continue
         if calculations == 0:
-            try:
-                first = float(parts[0])
-                second = float(parts[2])
-            except ValueError:
-                print("Please enter a valid number\n")
-                continue    
-
-            first = float(parts[0])
-            operator = parts[1]
-            second = float(parts[2])
+            if len(parts) == 3:
+                try:
+                    first = float(parts[0])
+                    operator = parts[1]
+                    second = float(parts[2])
+                except ValueError:
+                    print("Please enter a valid number\n")
+                    continue
+            else:
+                print("Please enter a valid operation. Or enter 'help' to get a list of useful commands.\n")    
+                continue 
           
         elif calculations > 0:
-            try:
-                first = float(result)
-                second = float(parts[1])
-            except ValueError:
-                print("Please enter a valid number\n")
-                continue    
-            first = result
-            operator = parts[0]
-            second = float(parts[1])
-     
+            if len(parts) == 2:
+                try:
+                    first = float(result)
+                    operator = parts[0]
+                    second = float(parts[1])
+                except ValueError:
+                    print("Please enter a valid number\n")
+                    continue
+            else:
+                print("Please enter a valid operation. Or enter 'help' to get a list of useful commands.\n") 
+                continue   
+            
         calculations = calculations + 1
         if operator == "+":
             result = add(first, second)
@@ -76,11 +95,15 @@ def calculator():
 
         elif operator == "/":
             if second == 0:
-                print("ERROR: Unable to divide by 0, please try again.")
+                print("ERROR: Unable to divide by 0, please try again.\n")
                 calculations = 0
                 continue
             result = divide(first, second)
             print(result)
-
-
+        elif operator in {"^", "**"}:
+            result = power(first, second)
+            print(result)
+        else:
+            print("Invalid operator, please try again. Or enter 'help' to get a list of useful commands.\n")
 calculator()
+
